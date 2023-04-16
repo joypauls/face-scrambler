e8b438654415c9d626bfc9d30538186334e347a0
joypauls <joypaulsen3@gmail.com>
2023-04-15 20:45:16 -0500

    init

--- src/App.js
+++ src/App.js
@@ -13,11 +13,26 @@ import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
 
 function App() {
+  // initialize canvas
+  var canvas = React.useRef();
+
+  React.useEffect(() => {
+    const context = canvas.current.getContext("2d");
+    const image = new Image();
+    // image.src =
+      // "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Picture_icon_BLACK.svg/1200px-Picture_icon_BLACK.svg.png";
+    image.src = "test.jpg"
+    image.onload = () => {
+      context.drawImage(image, 0, 0, 500, 500);
+    };
+  }, []);
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
+          <canvas ref={canvas} width={500} height={500} />
           <VStack spacing={8}>
             <Logo h="40vmin" pointerEvents="none" />
             <Text>
